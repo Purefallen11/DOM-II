548ac5279f06e4779aba488926a8bd913098f9f8
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-07 00:22:17 -0500

    added a hover effect to top nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const navHover = document.querySelectorAll(".logo-heading, .nav-link")
+
+navHover.forEach(item => {
+    item.addEventListener("mouseenter", e => {
+        e.target.style.color = "purple"
+    })
+})
+
+navHover.forEach(item => {
+
+    item.addEventListener("mouseout", e => {
+        e.target.style.color = "black"
+    })
+})
