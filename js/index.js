8d36af7ffed6c83642722f5acf31a384b87bcaab
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-07 19:05:57 -0500

    added a few fun eventlisteners, i did not, however, add at least 10

--- js/index.js
+++ js/index.js
@@ -13,3 +13,29 @@ navHover.forEach(item => {
         e.target.style.color = "black"
     })
 })
+
+const konami = document.querySelector("body")
+
+konami.addEventListener("keydown", (e) => {
+    const keyName = e.keyCode;
+    const playSound = () => {
+        let sound = new Audio(
+            "https://vgmsite.com/soundtracks/teenage-mutant-ninja-turtles-tournament-fighters-snes-audio/zepykxueeo/01%20Konami%20Fanfare.mp3"
+        );
+        sound.play();
+    }
+    
+    if (keyName === 65) {
+       playSound()
+    }
+})
+
+const easterEgg = document.querySelector(".intro h2")
+
+easterEgg.addEventListener("mouseover", (e) => {
+    e.target.textContent = "Press A"
+})
+
+easterEgg.addEventListener("mouseout", (e) => {
+    e.target.textContent = "Welcome To Fun Bus!"
+})
